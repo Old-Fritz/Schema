3ab7c04a3caa0f789cd7c876aaf07d283eda5c87
Mikhail Gostev <mv.gostev@skbkontur.ru>
2019-05-24 17:09:12 +0300

    Correct bist module

--- schemotecnics_lab2.srcs/sources_1/new/bist.v
+++ schemotecnics_lab2.srcs/sources_1/new/bist.v
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ps
+cale 1ns / 1ps
 
 module bist(
     input clk_i, // Тактовый сигнал
@@ -8,38 +8,37 @@ module bist(
     
     output reg [0:0] test_mode_o, // Информация о режиме тестирования: 0 - обычный режим, 1 - режим тестирования
     output reg [7:0] switch_count_o, // Количество переключерний в режим тестирования
-    output busy_o, // Занятость платы в данный момент
-    output integer test_index_o // Номер последнего выполненного теста; Если ни одного теста еще не выполнилось, то имеет значение -1
+    output reg step_signal_o // Занятость платы в данный момент
 );
 
     localparam TESTS_AMOUNT = 8'd255;
     
-    assign busy_o = (test_mode_o) ? test_index_o != -1 : function_busy_i;
+    integer test_index = 0;
       
     always@(negedge mode_switch_i)
         if(!test_mode_o) begin
             test_mode_o <= 1;
             switch_count_o <= switch_count_o + 1;
-        end else if (!busy_o) begin
-            test_index_o <= -1;
+        end else if (!step_signal_o) begin
+            test_index <= -1;
             test_mode_o <= 0;
         end
         
     always@(posedge clk_i)
         if(rst_i) begin
             test_mode_o <= 0;
-            test_index_o <= -1;
+            test_index <= -1;
             switch_count_o <= 0;
         end else if(test_mode_o) begin
-            
             if(!function_busy_i) begin
-                test_index_o <= test_index_o + 1;
-            
-                if(test_index_o >= TESTS_AMOUNT) begin
-                    test_index_o = -1;
+                if(test_index >= TESTS_AMOUNT - 1) begin
+                    test_index <= 1;
+                    step_signal_o <= 0;
+                end else begin
+                    step_signal_o <= 1;
+                    test_index <= test_index + 1;
                 end
-            end
-                
+            end        
         end
         
 endmodule
